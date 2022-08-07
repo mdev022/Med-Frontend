d79d804bc68fad8f83c7d365dc731be883be1bb5
Koustav <koustavlmcr17@gmail.com>
2022-08-08 00:42:41 +0530

    Added footer

--- src/components/Advanced/Footer/index.tsx
+++ src/components/Advanced/Footer/index.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
 import React from 'react';
+import MailIconRed from '../../SvgComponents/MailIconRed';
 import MainLogo from '../../SvgComponents/MainLogo';
+import PhoneIconRed from '../../SvgComponents/PhoneIconRed';
 import styles from "./index.module.scss";
 
 export default function Footer() {
@@ -11,13 +14,37 @@ export default function Footer() {
             <MainLogo />
             <h1>Medinery</h1>
           </div>
+          <p>
+            A platform for doctors to showcase their own selves and create a digital presence.
+          </p>
         </div>
         <div className={styles.footerCol2}>
-            
-
+          <h3>Reach Us</h3>
+          <p>Please contact below details for any other information.</p>
+          <div>
+            <span><MailIconRed />  medinery2020@gmail.com</span>
+          </div>
+          <div>
+            <span><PhoneIconRed />  +917602022864</span>
+          </div>
         </div>
         <div className={styles.footerCol3}>
-
+          <h3>Legal</h3>
+          <div>
+            <Link href="/">
+              Terms and conditions
+            </Link>
+          </div>
+          <div>
+            <Link href="/">
+              Refund Policy
+            </Link>
+          </div>
+          <div>
+            <Link href="/">
+              Information privacy.
+            </Link>
+          </div>
         </div>
       </div>
     </div>
