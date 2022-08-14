52d10c442022c90c8cd80d14685eee4be01e7d7c
Koustav <koustavlmcr17@gmail.com>
2022-08-14 16:56:42 +0530

    added favicon and sharp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <title>Medinery</title>
         <meta name="description" content="Medinery" />
