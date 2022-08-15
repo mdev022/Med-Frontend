d66d06aa4b6c914d3bd43040f045e60fd98e21d8
Koustav <koustavlmcr17@gmail.com>
2022-08-15 14:10:42 +0530

    Added profile theme V2

--- src/components/Basic/ConnectSection/ConnectSection.tsx
+++ src/components/Basic/ConnectSection/ConnectSection.tsx
@@ -9,29 +9,34 @@ import styles from "./index.module.scss";
 interface IConnectionSectionProps{
     headerWithShadow?: boolean;
     styleClass?:string;
+    customHeaderText?:string;
+    data:any
 
 }
 
 export default function ConnectSection({
   headerWithShadow,
-  styleClass
+  data,
+  styleClass,
+  customHeaderText
 }: IConnectionSectionProps) {
+  const headerText = customHeaderText || "Connect";
   const ConnectSectionHeader: () => JSX.Element = () => {
-    return headerWithShadow ?  <h1 className='headerWithShadow'>Connect<span>Connect</span></h1> : <h1>Connect</h1>;
+    return headerWithShadow ?  <h1 className='headerWithShadow'>{headerText}<span>{headerText}</span></h1> : <h1>{headerText}</h1>;
   };
 
   return (
-    <div className={`${styles.wrapper} container `}>
-      <ConnectSectionHeader />
-      <div className={`${styles.connectInfoWrapper}`}>
-        <div className={``}><MailIconRed /> <span>kunalsarkar12@gmail.com</span></div>
-        <div className={``}><PhoneIconRed /> <span>+91 9416846189</span></div>
+    <div className={`${styles.wrapper} container ${styleClass}__wrapper `}>
+      <ConnectSectionHeader  />
+      <div className={`${styles.connectInfoWrapper} ${styleClass}__infowrapper`}>
+        <div className={``}><MailIconRed /> <span>{data?.display_email}</span></div>
+        <div className={``}><PhoneIconRed /> <span>{data?.display_number}</span></div>
       </div>
-      <div className={`${styles.connectWithMeWrapper}`} >
-        <h4>Or connect with me</h4>
-        <div><FacebookIconDark/>
-          <YoutubeIconDark />  
-          <TwitterIconDark /> 
+      <div className={`${styles.connectWithMeWrapper} ${styleClass}__socialswrapper`} >
+        <h5>Or connect with me</h5>
+        <div>  <a href={data?.socials?.facebook_link}><FacebookIconDark /></a>
+          <a href={data?.socials?.youtube_link}> <YoutubeIconDark /></a>
+          <a href={data?.socials?.twitter_link}><TwitterIconDark /></a>
         </div>
       </div>
     </div>
