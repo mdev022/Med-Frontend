33ffe4bd596b575130e79c787ed693cbcd62525c
Koustav <koustavlmcr17@gmail.com>
2022-09-03 23:59:18 +0530

    Navbar search dropdown bug fixed and subscribe email input in footer

--- src/components/Advanced/Footer/index.tsx
+++ src/components/Advanced/Footer/index.tsx
@@ -1,11 +1,27 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import AxiosApi from '../../../config/apiAxios';
+import apiOptions, { SUBSCRIBE_WITH_EMAIL } from '../../../config/endpoints';
 import MailIconRed from '../../SvgComponents/MailIconRed';
 import MainLogo from '../../SvgComponents/MainLogo';
 import PhoneIconRed from '../../SvgComponents/PhoneIconRed';
 import styles from "./index.module.scss";
 
 export default function Footer() {
+  const [email,setEmail] = useState<string>("");
+  const [submitting,setSubmitting] = useState<boolean>(false);
+  const handleEmailSubmit = async () => {
+    setSubmitting(true);
+    try{
+      const res = await AxiosApi({...apiOptions(SUBSCRIBE_WITH_EMAIL), data:{email:email}});
+      
+    }catch(err){
+      console.log(err);
+    }finally{
+      setSubmitting(false);
+    }
+  };
+
   return (
     <div className={styles.footerContainer}>
       <div className='container'>
@@ -29,8 +45,21 @@ export default function Footer() {
           </div>
         </div>
         <div className={styles.footerCol3}>
-          <h3>Legal</h3>
-          <div>
+          <h3>Stay Connected</h3>
+          <p>Drop your mail if you are interested in our services and would like to get updates and not miss out anything</p>
+          <div className={styles.mailInputWrapper}>
+            <input className={styles.mailInput} 
+              placeholder="Drop your mail here" 
+              type="email" 
+              value={email} 
+              onChange={(e)=> setEmail(e.target.value)}
+            />
+            {
+              submitting ? <div className="spinner-border text-success" role="status"></div> 
+                : <button onClick={handleEmailSubmit}>Submit</button>
+            }
+          </div>
+          {/* <div>
             <Link href="/">
               Terms and conditions
             </Link>
@@ -44,7 +73,7 @@ export default function Footer() {
             <Link href="/">
               Information privacy.
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
